b9b03735d249930ed3cf5293c84eed6f82f588bf
heruts <37506075+heruts@users.noreply.github.com>
2025-12-08 22:31:57 +0200

    update TA7

--- TA7/QSolution/Interface1.java
+++
@@ -1,5 +0,0 @@
-package QSolution;
-
-public interface Interface1<R,T> {
-    R compute(T param1, T param2);
-}
--- TA7/QSolution/Interface2.java
+++
@@ -1,5 +0,0 @@
-package QSolution;
-
-public interface Interface2<T> {
-    T compute(T param1, T param2);
-}
